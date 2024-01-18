fe630afd5fbca033ef4b336680d3de3378b594d0
Lutti Davide <luttidavide004@gmail.com>
2024-01-18 15:03:34 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student("Mario", "Rossi", 1);
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Surname: " + student.getSurname());
+        System.out.println("Identification Number: " + student.getIdentificationNumber());
+
+        student.setName("Giovanni");
+        student.setSurname("Verdi");
+        student.setIdentificationNumber(56);
+
+        System.out.println("Updated name: " + student.getName());
+        System.out.println("Updated surname: " + student.getSurname());
+        System.out.println("Updated identification number: " + student.getIdentificationNumber());
+    }
+}
